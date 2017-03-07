3b0642ee89dcabef5ed50bd28eec364bb77b0045
Sky Rolnick <skymeson@gmail.com>
2017-03-07 15:45:52 -0500

    First Commit
    Solor System Simulator

---
+++ Assets/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float turnSpeed = 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        
+    }
+}
---
+++ Assets/RotatePivot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePivot : MonoBehaviour {
+
+    public float turnSpeed = 10.0f;
+    public Vector3 axis = new Vector3(0, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(axis, Vector3.up, - turnSpeed * Time.deltaTime);
+	}
+}
